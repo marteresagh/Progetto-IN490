5f51d4ee1ba46e030241bdcd9c935f372f135e68
ValeriaPrevete <38942167+ValeriaPrevete@users.noreply.github.com>
2018-12-21 10:44:43 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,13 +2,15 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		Grafo g=new Grafo();
-		
-		for(int i=1; i<50; i++) {
-			System.out.println("sto nel giro"+ i);
-			g.random();
+	public static void main(String[] args) { // bisogna passargli in input il numero massimo di vertici ed il numero di archi
+		Grafo g=new Grafo();		 
+		int n = Integer.parseInt(args[0]);//numero massimo di vertici	
+		int m = Integer.parseInt(args[1]);//numero di archi
+		for(int i=0; i<m; i++) {
+			System.out.println("sto nel giro "+ i);
+			g.random(n);
 		}
+			
 			
 	}
 		
